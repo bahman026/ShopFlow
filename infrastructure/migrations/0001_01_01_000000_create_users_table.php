6aaa8486f97934419d62480fd8a4f8134d412a9c
bahman <bahman026@gmail.com>
2024-08-01 09:47:28 +0330

    update admin migration

--- infrastructure/migrations/0001_01_01_000000_create_users_table.php
+++ infrastructure/migrations/0001_01_01_000000_create_users_table.php
@@ -15,9 +15,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('mobile',11)->nullable();
+            $table->timestamp('mobile_verified_at')->nullable();
+            $table->string('national_id',10)->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
