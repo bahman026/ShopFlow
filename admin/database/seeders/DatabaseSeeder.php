9b5ff01ba858d0bffc7c994adfe68ab2de6e6d7f
bahman <bahman026@gmail.com>
2024-08-07 09:09:49 +0330

    implement city seeder

--- admin/database/seeders/DatabaseSeeder.php
+++ admin/database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RolePermissionSeeder::class,
             AdminSeeder::class,
+            CitySeeder::class,
         ]);
     }
 }
