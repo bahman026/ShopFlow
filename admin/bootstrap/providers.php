a59779b366d435a156d5a84028961314b11af9b7
Bahman Jafarzadeh <bja@adlas.at>
2025-08-09 18:31:26 +0330

    feat(admin): add FilamentServiceProvider to providers list
    - Include FilamentServiceProvider in the list of service providers
    - This addition is essential for integrating Filament functionality into the admin area
    
    Signed-off-by: Bahman Jafarzadeh <bja@adlas.at>

--- admin/bootstrap/providers.php
+++ admin/bootstrap/providers.php
@@ -5,4 +5,5 @@ declare(strict_types=1);
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
+    App\Providers\FilamentServiceProvider::class,
 ];
