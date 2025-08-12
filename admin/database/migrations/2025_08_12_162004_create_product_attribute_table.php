9661ffd02aef3e52b969dc1d374fe48fd200bd84
Bahman026 <Bahman026@gmail.com>
2025-08-12 21:25:20 +0330

    build(admin): replace Filament Tiptap Editor with TinyEditor
    - Remove awcodes/filament-tiptap-editor
    - Add mohamedsabil83/filament-forms-tinyeditor

---
+++ admin/database/migrations/2025_08_12_162004_create_product_attribute_table.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Models\Attribute;
+use App\Models\Product;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_attribute', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Product::class);
+            $table->foreignIdFor(Attribute::class);
+            $table->boolean('is_highlight')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_attribute');
+    }
+};
